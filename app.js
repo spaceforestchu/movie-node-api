64980753c6e20963cf13363d6b53ff7d4e490904
spaceforestchu <spaceforestchu@gmail.com>
2018-07-11 18:59:37 -0400

    take engine out

--- app.js
+++ app.js
@@ -28,8 +28,6 @@ var apiRouter = require('./routes/api');
 
 var app = express();
 
-app.engine('ejs', engine);
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
